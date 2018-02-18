532a7a70b0fcc69ecdc8556eca901e3dee12f254
Peter Postmann <peter@postmann.org>
2018-02-18 21:03:01 +0100

    .

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "0: $0"
+
 script_dir_realtive=${0%/*}
 script_dir=$(pwd)$([ ! -z "$script_dir_realtive" ] && printf "%s" "/$script_dir_realtive")
+
+echo "script_dir: $script_dir"
+echo "OSTYPE: $OSTYPE"
 
 case "$OSTYPE" in
     darwin*)  OS="darwin";  ;;
@@ -14,6 +19,8 @@ esac;
 
 # Add module binaries to PATH
 PATH="$PATH:$script_dir/bin/$OS"
+
+echo "PATH: $PATH"
 
 # Check if jq is present
 if ! [ -x "$(command -v jq 2>/dev/null)" ]; then
