bf0c92111eb032a47b18f39c44ef8d5823511e37
Peter Postmann <peter@postmann.org>
2018-02-28 14:03:26 +0100

    .

--- tests/test.sh
+++ tests/test.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+# Get Packages
+./sbpl.sh
+
+# Include Packages
+export PATH="$(./sbpl.sh envvars sbpl_path_bin):$PATH"
+
+
+### Run Test
+
 # Check if Terraform is present
 if ! [ -x "$(command -v terraform 2>/dev/null)" ]; then
-    
-    if ! [ -f terraform ]; then
-        echo 'error: terraform not found'
-        exit 2
-    else
-        function terraform () {
-            ./terraform $@
-        }
-    fi
+    echo 'error: terraform not found in $PATH'
+    exit 2
 fi
 
 # Init Terraform
@@ -25,5 +27,10 @@ if terraform apply -auto-approve -var 'color=cyan' | grep "value = #0ff" > /dev/
 else
     echo "fail"
     exit 1
+fi
+
+### Run Wine
+if command -v wine; then
+    wine cmd /c "set PATH=%cd%\\vendor\\bin\\windows\\386;%PATH% && test.bat"
 fi
 
